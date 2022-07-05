a318388fb0e01dab00a8dd3a7c42e7cb274e8d7e
michael-franke <the.secret.goldfish@me.com>
2022-07-05 19:13:43 +0200

    bug fixing, linting

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,41 @@
 <template>
-<Experiment title="magpie demo">
-  <InstructionScreen :title="'Welcome'">
-    This is a sample introduction screen.
-  </InstructionScreen>
+  <Experiment title="magpie demo">
+    <InstructionScreen :title="'Welcome'">
+      This is a sample introduction screen.
+    </InstructionScreen>
 
-  <template v-for="(trial, i) in trials">
-    <Screen>
-
-      <Slide>
-
-      <Record
-        :data="{
-          trialNr: i + 1,
-          itemName: trial.itemName,
-          settingName: trial.settingName
-        }"
-      />
-
-        <span
-         v-for="(line,lineNumber) of trial.vignette.split('\\n')"
-         v-bind:key="lineNumber" >
-         {{ line }}<br/>
-        </span>
-
-        <TextareaInput
-          :response.sync= "$magpie.measurements.answer"
+    <template v-for="(trial, i) in trials">
+      <Screen :key="i">
+        <Slide>
+          <Record
+            :data="{
+              trialNr: i + 1,
+              itemName: trial.itemName,
+              settingName: trial.settingName
+            }"
           />
 
-        <button v-if= "$magpie.measurements.answer && $magpie.measurements.answer.length > 2"
-                @click="$magpie.saveAndNextScreen();">
-          Submit
-        </button>
-      </Slide>
+          <span
+            v-for="(line, lineNumber) of trial.vignette.split('\\n')"
+            :key="lineNumber"
+          >
+            {{ line }}<br />
+          </span>
 
-    </Screen>
+          <TextareaInput :response.sync="$magpie.measurements.answer" />
 
-</template>
+          <button
+            v-if="
+              $magpie.measurements.answer &&
+              $magpie.measurements.answer.length > 2
+            "
+            @click="$magpie.saveAndNextScreen()"
+          >
+            Submit
+          </button>
+        </Slide>
+      </Screen>
+    </template>
 
     <SubmitResultsScreen />
   </Experiment>
@@ -47,18 +47,18 @@ import trialsAll from '../trials/trials.csv';
 
 var group = _.sample(['odd', 'even']);
 
-
-const trials = group == 'odd' ?
-      trialsAll.filter((element, index) => {
-        return (index % 2 === 0);
-      }) :
-      trialsAll.filter((element, index) => {
-        return (index % 2 != 0);
+const trials =
+  group == 'odd'
+    ? trialsAll.filter((element, index) => {
+        return index % 2 === 0;
+      })
+    : trialsAll.filter((element, index) => {
+        return index % 2 != 0;
       });
 
-console.log(group, trials)
+console.log(group, trials);
 
-console.log('test')
+console.log('test');
 
 export default {
   name: 'App',
