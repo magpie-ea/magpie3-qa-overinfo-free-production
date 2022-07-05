2af958136f7effd11d282a86afada147f318f24d
michael-franke <the.secret.goldfish@me.com>
2022-07-05 18:45:56 +0200

    added trials, github-actions; 1st trial structure

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,33 @@
 <template>
-  <Experiment title="magpie demo">
-    <InstructionScreen :title="'Welcome'">
-      This is a sample introduction screen.
-    </InstructionScreen>
+<Experiment title="magpie demo">
+  <InstructionScreen :title="'Welcome'">
+    This is a sample introduction screen.
+  </InstructionScreen>
+
+  <template v-for="(trial, i) in trials">
+    <Screen>
+
+      <Slide>
+
+        <span
+         v-for="(line,lineNumber) of trial.vignette.split('\\n')"
+         v-bind:key="lineNumber" >
+         {{ line }}<br/>
+        </span>
+
+        <TextareaInput
+          :response.sync= "$magpie.measurements.answer"
+          />
+
+        <button v-if= "$magpie.measurements.answer && $magpie.measurements.answer.length > 2"
+                @click="$magpie.saveAndNextScreen();">
+          Submit
+        </button>
+      </Slide>
+
+    </Screen>
+
+</template>
 
     <SubmitResultsScreen />
   </Experiment>
@@ -10,11 +35,14 @@
 
 <script>
 import _ from 'lodash';
+import trials from '../trials/trials.csv';
 
 export default {
   name: 'App',
   data() {
-    return {};
+    return {
+      trials: trials
+    };
   },
   computed: {
     // Expose lodash to template code
