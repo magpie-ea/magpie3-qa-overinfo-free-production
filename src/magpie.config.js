e72001d3b4be5dfcd650de65b8cb45f43c153c55
Marcel Klehr <mklehr@gmx.net>
2022-07-06 21:01:42 +0200

    magpie.config.js: Remove socketURL

--- src/magpie.config.js
+++ src/magpie.config.js
@@ -1,7 +1,6 @@
 export default {
   experimentId: '89',
   serverUrl: 'https://mcmpact.ikw.uni-osnabrueck.de/magpie/',
-  socketUrl: 'wss://mcmpact.ikw.uni-osnabrueck.de/magpie/socket',
   // this will be used in prolific mode
   completionUrl: 'https://app.prolific.co/submissions/complete?cc=87604CC5',
   contactEmail: 'michael.franke@uni-tuebingen.de',
