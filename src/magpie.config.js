4a2bdf6fdebb899a09175e374ddca56a2b5438d6
polina-tsvilodub <polina.tsvilodub@gmail.com>
2022-10-22 19:26:57 +0200

    add extended trials

--- src/magpie.config.js
+++ src/magpie.config.js
@@ -1,10 +1,10 @@
 export default {
-  experimentId: '89',
+  experimentId: '95',
   serverUrl: 'https://mcmpact.ikw.uni-osnabrueck.de/magpie/',
   // this will be used in prolific mode
   completionUrl: 'https://app.prolific.co/submissions/complete?cc=87604CC5',
-  contactEmail: 'michael.franke@uni-tuebingen.de',
+  contactEmail: 'polna.tsvilodub@gmail.com',
   // Either 'debug', 'directLink' or 'prolific'
-  mode: 'prolific',
+  mode: 'directLink',
   language: 'en'
 };
