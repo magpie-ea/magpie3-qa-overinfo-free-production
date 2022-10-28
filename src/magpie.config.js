77cdc1366cd318a50e656e66054fad36219e5e16
polina-tsvilodub <polina.tsvilodub@gmail.com>
2022-10-28 08:32:46 +0200

    add prolific url

--- src/magpie.config.js
+++ src/magpie.config.js
@@ -2,9 +2,9 @@ export default {
   experimentId: '95',
   serverUrl: 'https://mcmpact.ikw.uni-osnabrueck.de/magpie/',
   // this will be used in prolific mode
-  completionUrl: 'https://app.prolific.co/submissions/complete?cc=87604CC5',
+  completionUrl: 'https://app.prolific.co/submissions/complete?cc=C1J3ETN9',
   contactEmail: 'polna.tsvilodub@gmail.com',
   // Either 'debug', 'directLink' or 'prolific'
-  mode: 'directLink',
+  mode: 'prolific',
   language: 'en'
 };
