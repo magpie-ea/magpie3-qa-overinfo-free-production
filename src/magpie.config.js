b78125fbddaa4c7f5848deac4d590709c91e3786
polina-tsvilodub <polina.tsvilodub@gmail.com>
2022-10-28 10:46:35 +0200

    add socket url

--- src/magpie.config.js
+++ src/magpie.config.js
@@ -1,6 +1,7 @@
 export default {
   experimentId: '95',
   serverUrl: 'https://mcmpact.ikw.uni-osnabrueck.de/magpie/',
+  socketUrl: 'wss://mcmpact.ikw.uni-osnabrueck.de/magpie/socket',
   // this will be used in prolific mode
   completionUrl: 'https://app.prolific.co/submissions/complete?cc=C1J3ETN9',
   contactEmail: 'polna.tsvilodub@gmail.com',
