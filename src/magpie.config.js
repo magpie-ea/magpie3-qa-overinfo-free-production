02ad7ea3d2749731db8ed02616676abb5d83f870
polina-tsvilodub <polina.tsvilodub@gmail.com>
2022-10-28 13:36:46 +0200

    remove socket url

--- src/magpie.config.js
+++ src/magpie.config.js
@@ -1,11 +1,11 @@
 export default {
   experimentId: '95',
   serverUrl: 'https://mcmpact.ikw.uni-osnabrueck.de/magpie/',
-  socketUrl: 'wss://mcmpact.ikw.uni-osnabrueck.de/magpie/socket',
+  // socketUrl: 'wss://mcmpact.ikw.uni-osnabrueck.de/magpie/socket',
   // this will be used in prolific mode
   completionUrl: 'https://app.prolific.co/submissions/complete?cc=C1J3ETN9',
   contactEmail: 'polina.tsvilodub@gmail.com',
   // Either 'debug', 'directLink' or 'prolific'
-  mode: 'directLink',
+  mode: 'prolific',
   language: 'en'
 };
