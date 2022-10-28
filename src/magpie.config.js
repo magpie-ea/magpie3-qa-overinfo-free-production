49a7ec8bdfe94ec8336126a4de57285ed2d4cd29
polina-tsvilodub <polina.tsvilodub@gmail.com>
2022-10-28 10:50:43 +0200

    fix typo

--- src/magpie.config.js
+++ src/magpie.config.js
@@ -4,7 +4,7 @@ export default {
   socketUrl: 'wss://mcmpact.ikw.uni-osnabrueck.de/magpie/socket',
   // this will be used in prolific mode
   completionUrl: 'https://app.prolific.co/submissions/complete?cc=C1J3ETN9',
-  contactEmail: 'polna.tsvilodub@gmail.com',
+  contactEmail: 'polina.tsvilodub@gmail.com',
   // Either 'debug', 'directLink' or 'prolific'
   mode: 'prolific',
   language: 'en'
