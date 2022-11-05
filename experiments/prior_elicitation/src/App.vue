e24e219cf0beeda909e43c4f1e0fc5e0db2f226e
polina-tsvilodub <polina.tsvilodub@gmail.com>
2022-11-05 11:26:38 +0100

    update server configs and submission screen

--- experiments/prior_elicitation/src/App.vue
+++ experiments/prior_elicitation/src/App.vue
@@ -48,7 +48,7 @@
 
     <PostTestScreen />
 
-    <DebugResultsScreen />
+    <SubmitResultsScreen />
 
   </Experiment>
 </template>
--- experiments/prior_elicitation/src/magpie.config.js
+++ experiments/prior_elicitation/src/magpie.config.js
@@ -1,11 +1,11 @@
 export default {
-  experimentId: '5',
-  serverUrl: 'https://magpie-temp.herokuapp.com/',
-  socketUrl: 'wss://magpie-temp.herokuapp.com/socket',
+  experimentId: '97',
+  serverUrl: 'https://mcmpact.ikw.uni-osnabrueck.de/magpie/',
+  socketUrl: 'wss://mcmpact.ikw.uni-osnabrueck.de/magpie/socket',
   // this will be used in prolific mode
   completionUrl: 'https://app.prolific.co/submissions/complete?cc=C15HJWQ2',
   contactEmail: 'polina.tsvilodub@gmail.com',
   // Either 'debug', 'directLink' or 'prolific'
-  mode: 'debug',
+  mode: 'prolific',
   language: 'en'
 };
