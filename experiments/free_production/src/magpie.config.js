92d7790cead4c9275b27cdd24c7cedde92d021dc
polina-tsvilodub <polina.tsvilodub@gmail.com>
2022-12-25 20:34:02 +0100

    update instructions, finalize items

---
+++ experiments/free_production/src/magpie.config.js
@@ -0,0 +1,11 @@
+export default {
+  experimentId: '103',
+  serverUrl: 'https://mcmpact.ikw.uni-osnabrueck.de/magpie/',
+  socketUrl: 'wss://mcmpact.ikw.uni-osnabrueck.de/socket',
+  // this will be used in prolific mode
+  completionUrl: 'https://app.prolific.co/submissions/complete?cc=C1J3ETN9',
+  contactEmail: 'polina.tsvilodub@uni-tuebingen.de',
+  // Either 'debug', 'directLink' or 'prolific'
+  mode: 'directLink',
+  language: 'en'
+};
