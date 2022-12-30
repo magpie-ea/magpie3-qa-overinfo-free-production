4f72defd3277511deaf4f3acd4d4f3955b9991b4
polina-tsvilodub <polina.tsvilodub@gmail.com>
2022-12-30 15:25:31 +0100

    update scorer for HF LMs

--- code/QA_models_answer_sampling.py
+++ code/QA_models_answer_sampling.py
@@ -115,8 +115,22 @@ def sample_response_from_lm(model, path, output_path, topk, num_beams=1, max_len
         Huggingface name of the model to be used.
     path: str
         Path to stimuli file.
-    output: str
+    output_path: str
         Path for saving file with samples.
+    topk: int
+        Number of predictions to return per input sample.
+    num_beams: int
+        Beam size rto use for returning >1 prediction per sample.
+    max_length: int
+        Maximal predicted sequence length.
+    temperature: float
+        Temperature for sampling next tokens.
+    top_p: float
+        P for nucleus sampling.
+    top_k: int
+        Number of tokens k to consider with top K sampling.
+    seed: int
+        Random seed to set.
     """
     torch.manual_seed(seed)
     random.seed(seed)
@@ -176,6 +190,89 @@ def sample_response_from_lm(model, path, output_path, topk, num_beams=1, max_len
     
     df.to_csv(output_path, index=False)  
 
+def score_answers_with_lm(model, path, output_path, reduction="mean", seed=1234):
+    """
+    Helper for computing the probability of given responses under a pretrained LM.
+
+    Arguments:
+    ----------
+    model: str
+        Huggingface name of the model to be used.
+    path: str
+        Path to stimuli file.
+    output_path: str
+        Path for saving file with samples.
+    seed: int
+        Random seed to set.
+    """
+    torch.manual_seed(seed)
+    random.seed(seed)
+
+    # read file with questions and answers
+    df = pd.read_csv(path)
+    # assume there is a question and answer field and the file has long format
+    assert all([c in df.columns for c in ["question", "context_qa", "taciturn", "competitor", "sameCategory", "otherCategory", "fullList"]]), "The stimuli file has to include the following columns: question, context_qa, taciturn, competitor, sameCategory, otherCategory, fullList"
+    # move to gpu for faster inference
+    if torch.backends.mps.is_available():
+        device = "mps"
+    elif torch.cuda.is_available():
+        device = "cuda"
+    else:
+        device = "cpu"
+
+    print("DEVICE ", device)
+
+    criterion = torch.nn.CrossEntropyLoss(reduction=reduction)
+    # load requested model
+    model_qa = transformers.AutoModelWithLMHead.from_pretrained(model)
+    tokenizer = transformers.AutoTokenizer.from_pretrained(model)
+    model_qa.to(device)
+    print("Model class ", model_qa.config_class)
+    print("Vocab size ", len(tokenizer))
+
+    model_name = []
+    predictions = []
+    answer_type = []
+    for i, r in tqdm(df.iterrows()):
+        probs = []
+        # iterate over the different response options
+        for a in ["taciturn", "competitor", "sameCategory", "otherCategory", "fullList"]:
+            prompt = "question: " + r["question"] + " context: " + r["context_qa"]
+            # tokenize
+            encoded_input = tokenizer(
+                [prompt],
+                return_tensors='pt',
+                max_length=512,
+                truncation=True
+            )
+            encoded_input.to(device)
+            encoded_answer = tokenizer(
+                [r[a]],
+                return_tensors='pt',
+                max_length=512,
+                truncation=True,
+            )
+            encoded_answer.to(device)
+            # make forward step
+            with torch.no_grad():
+                output = model_qa(input_ids = encoded_input.input_ids, labels = encoded_answer.input_ids)
+            # manually retrieve NLL from CE computation (to allow flexibility with respect to the reduction strategy)
+            nll = criterion(output.logits.transpose(1,2), encoded_answer.input_ids)
+            # store results (probs)
+            probs.append(torch.exp(-nll).item())
+            model_name.append(model)
+            answer_type.append(a)
+        # normalize over the different answer types
+        vignette_probs = probs/np.sum(probs)
+        predictions.append(vignette_probs)
+    # append results to overall df
+    df["answer_type_prob"] = predictions
+    df = df.explode(["answer_type_prob"])
+    df["model_name"] = model_name
+    df["answer_type"] = answer_type
+    
+    df.to_csv(output_path)
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-m", "--model", help = "name of the Huggingface model to be used. If it cannot be accessed, an error will be thrown.")
@@ -193,6 +290,7 @@ if __name__ == "__main__":
     parser.add_argument("-tp", "--top_p", help = "top p for nucleus sampling when doing LM sampling", nargs="?", default=1, type=float)
     parser.add_argument("-tk", "--top_k", help = "top k number of tokens for sampling when doing LM sampling", nargs="?", default=0, type=int)
     parser.add_argument("-s", "--seed", help = "random seed for drawing several samples", nargs="?", default=1234, type=int)
+    parser.add_argument("-r", "--reduction", help = "cross entropy calculation reduction strategy for scoring answer options with LMs", nargs="?", default="mean", type=str, choices=["mean", "sum"])
 
     args = parser.parse_args()
 
@@ -200,7 +298,9 @@ if __name__ == "__main__":
         predict_from_qa_model(args.model, args.path, args.output, args.topk, args.max_length)
 
     elif args.task == "lm_sampling":
-        sample_response_from_lm(args.model, args.path, args.output, args.topk, args.num_beams, args.max_length, args.temperature, args.top_p, args.top_k)
+        sample_response_from_lm(args.model, args.path, args.output, args.topk, args.num_beams, args.max_length, args.temperature, args.top_p, args.top_k, args.seed)
 
+    elif args.task == "lm_prob":
+        score_answers_with_lm(args.model, args.path, args.output, args.reduction, args.seed)
     else:
         raise ValueError("Unrecognized task type")
