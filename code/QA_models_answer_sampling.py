3d21105554c0b39cfd0eb5a5435ff6269d4f8339
polina-tsvilodub <polina.tsvilodub@gmail.com>
2023-02-07 09:04:25 +0100

    clean up

--- code/QA_models_answer_sampling.py
+++ code/QA_models_answer_sampling.py
@@ -9,7 +9,6 @@ import pandas as pd
 from tqdm import tqdm
 import random
 import argparse
-# from pipelines import pipeline
 import numpy as np
 
 def correct_answer(answer):
@@ -55,6 +54,7 @@ def predict_from_qa_model(model, stimuli, output_path, topk, max_length):
     # query the model
     model_name = []
     predicted_spans = []
+    span_probs = []
     softmax = torch.nn.Softmax(dim=-1) 
     for _, r in tqdm(df[["context_qa", "question"]].iterrows()):
         # get token ids and position embeddings (contexts vs question) concatenated with CLS and SEP tokens
@@ -64,7 +64,6 @@ def predict_from_qa_model(model, stimuli, output_path, topk, max_length):
             return_tensors="pt"
         )
         inputs = encoding['input_ids']  # Token embeddings
-        # segment_embedding = encoding['token_type_ids']  # Segment embeddings
         tokens = tokenizer.convert_ids_to_tokens(inputs.squeeze(0)) # input tokens for later decoding
         tokens = [t.encode("ascii", "ignore").decode() for t in tokens]
         out = model_qa(inputs)
@@ -77,31 +76,41 @@ def predict_from_qa_model(model, stimuli, output_path, topk, max_length):
             ends = ends[None]
         
         ##### top k infrastructure (from QA pipeline huggingface src)
+        ##### with additional renormalization of probabilities of possible candidate spans to allow to retrieve chosen span probabilities
         outer = np.matmul(np.expand_dims(starts, -1), np.expand_dims(ends, 1))
         max_answer_len = max_length if max_length > 0 else len(r["context_qa"].split())
         # Remove candidate with end < start and end - start > max_answer_len
         candidates = np.tril(np.triu(outer), max_answer_len - 1)
         scores_flat = candidates.flatten()
+        # renormalize the possible candidate probabilities 
+        candidates_renorm = softmax(torch.tensor(scores_flat)).detach().numpy()
+        
         if topk == 1:
-            idx_sort = [np.argmax(scores_flat)]
+            idx_sort = [np.argmax(candidates_renorm)]
         elif len(scores_flat) < topk:
-            idx_sort = np.argsort(-scores_flat)
+            idx_sort = np.argsort(-candidates_renorm)
         else:
-            idx = np.argpartition(-scores_flat, topk)[0:topk]
-            idx_sort = idx[np.argsort(-scores_flat[idx])]
+            idx = np.argpartition(-candidates_renorm, topk)[0:topk]
+            idx_sort = idx[np.argsort(-candidates_renorm[idx])]
             
         start, end = np.unravel_index(idx_sort, candidates.shape)[1:]
         indices = zip(start, end)
         answers = [" ".join(tokens[start_index:(end_index+1)]) for start_index, end_index in indices]
         corrected_answers = [correct_answer(a) for a in answers]
+        # retrieving topk probs
+        spans_prob = candidates_renorm[idx_sort]
+        
+        span_probs.append(spans_prob)
         model_name.append(model)
         predicted_spans.append(corrected_answers)
+
         
 
     df["model_name"] = model_name
     df["predicted_spans"] = predicted_spans
+    df["prediction_probs"] = span_probs
 
-    df = df.explode(["predicted_spans"])
+    df = df.explode(["predicted_spans", "prediction_probs"])
     
     df.to_csv(output_path, index=False)    
 
@@ -168,6 +177,7 @@ def sample_response_from_lm(model, path, output_path, topk, num_beams=1, max_len
     few_shot_answer = "You reply: I'm sorry, I don't have any grilled zucchini. But I do have some grilled potatoes. Now consider a different situation."
     model_name = []
     predictions = []
+    probs = []
     for i, r in tqdm(df[["context_qa", "question"]].iterrows()):
         question = r["question"] 
         context = r["context_qa"] 
@@ -198,20 +208,28 @@ def sample_response_from_lm(model, path, output_path, topk, num_beams=1, max_len
                 temperature = temperature if temperature != 1 else 1.0,
                 top_p = top_p if top_p != 1 else top_p,
                 top_k = top_k if top_k != 0 else 50,
+                output_scores = True,
+                return_dict_in_generate = True,
             )
-            output = tokenizer.batch_decode(output, skip_special_tokens=True)
+            if "gpt2" in model:
+                # only take the scores of the answer sequence
+                start_ind = encoded_input.input_ids[0].shape[-1] + 1
+                output_seq = tokenizer.batch_decode(output.sequences[:, start_ind:], skip_special_tokens=True)
+            else:
+                output_seq = tokenizer.batch_decode(output.sequences, skip_special_tokens=True)
         # catch some weird (probably version difference related) bugs arising with finetuned BART
         except ValueError:
-            print("---- skipping vignette ----")
-            output = ""
+            output_seq = ""
         # append predictions
         model_name.append(model)
-        predictions.append(output)
+        predictions.append(output_seq)
+        probs.append(torch.exp(output.sequences_scores).tolist())
 
     df["model_name"] = model_name
     df["predictions"] = predictions
+    df["probs"] = probs
 
-    df = df.explode(["predictions"])
+    df = df.explode(["predictions", "probs"])
     
     df.to_csv(output_path, index=False)  
 
@@ -235,8 +253,19 @@ def score_answers_with_lm(model, path, output_path, reduction="mean", seed=1234)
 
     # read file with questions and answers
     df = pd.read_csv(path)
+    # colnames = ["taciturn", "competitor", "sameCategory1", "sameCategory2", "sameCategory3", "otherCategory", "fullList_0", "fullList_1", "fullList_2", "fullList_3", "fullList_4", "fullList_5"]
+    colnames = ["taciturn", "competitor", 'mostSimilar', "otherCategory", 'sameCategory_0', 'sameCategory_1', 'sameCategory_2', 'sameCategory_3',
+       'sameCategory_4', 'sameCategory_5', 'sameCategory_6', 'sameCategory_7',
+       'sameCategory_8', 'sameCategory_9', 'sameCategory_10',
+       'sameCategory_11', 'sameCategory_12', 'fullList_0', 'fullList_1',
+       'fullList_2', 'fullList_3', 'fullList_4', 'fullList_5', 'fullList_6',
+       'fullList_7', 'fullList_8', 'fullList_9', 'fullList_10', 'fullList_11',
+       'fullList_12', 'fullList_13', 'fullList_14', 'fullList_15',
+       'fullList_16', 'fullList_17', 'fullList_18', 'fullList_19',
+       'fullList_20', 'fullList_21', 'fullList_22', 'fullList_23']
+    
     # assume there is a question and answer field and the file has long format
-    assert all([c in df.columns for c in ["question", "context_qa", "taciturn", "competitor", "sameCategory", "otherCategory", "fullList"]]), "The stimuli file has to include the following columns: question, context_qa, taciturn, competitor, sameCategory, otherCategory, fullList"
+    assert all([c in df.columns for c in colnames + ["question", "context_qa"]]), "The stimuli file has to include the following columns: question, context_qa, taciturn, competitor, sameCategory, otherCategory (_1, _2, _3), fullList (_0 until _5)"
     # move to gpu for faster inference
     if torch.backends.mps.is_available():
         device = "mps"
@@ -248,6 +277,7 @@ def score_answers_with_lm(model, path, output_path, reduction="mean", seed=1234)
     print("DEVICE ", device)
 
     criterion = torch.nn.CrossEntropyLoss(reduction=reduction)
+    criterion_sum = torch.nn.CrossEntropyLoss(reduction="sum")
     # load requested model
     model_qa = transformers.AutoModelWithLMHead.from_pretrained(model)
     tokenizer = transformers.AutoTokenizer.from_pretrained(model)
@@ -257,11 +287,17 @@ def score_answers_with_lm(model, path, output_path, reduction="mean", seed=1234)
 
     model_name = []
     predictions = []
+    perplexities = []
+    # length_normalized_probs = []
     answer_type = []
+        
     for i, r in tqdm(df.iterrows()):
-        probs = []
+        # init named tuples
+        probs = {k: 0 for k in colnames}
+        ppls = {k: 0 for k in colnames}
+        normalized_probs = {k: 0 for k in colnames}
         # iterate over the different response options
-        for a in ["taciturn", "competitor", "sameCategory", "otherCategory", "fullList"]:
+        for a in colnames:
             if model == "danyaljj/gpt2_question_answering_squad2":
                 prompt = f"{r['context_qa']} Q: {r['question']} A: {r[a]}"
             elif model == "gpt2":
@@ -283,7 +319,6 @@ def score_answers_with_lm(model, path, output_path, reduction="mean", seed=1234)
                 truncation=True,
             )
             encoded_answer.to(device)
-            # make forward step
             with torch.no_grad():
                 # for decoder only models, the entire sequence including the context is passed as the label
                 if "gpt2" in model: 
@@ -294,22 +329,57 @@ def score_answers_with_lm(model, path, output_path, reduction="mean", seed=1234)
             # manually retrieve NLL from CE computation (to allow flexibility with respect to the reduction strategy)
             if "gpt2" in model:
                 # only take the scores of the answer sequence
-                start_ind = encoded_input.input_ids[0].tolist().index(encoded_answer.input_ids[0, 1].item()) # remove sos token
-                nll = criterion(output.logits[:, start_ind:, :].transpose(1,2), encoded_answer.input_ids[:, 1:])
+                encoded_input_rev = encoded_input.input_ids[0].tolist()
+                encoded_input_rev.reverse() # for finding last occurence of : as the token for retrieving the answer probs only
+                start_ind = len(encoded_input_rev) - encoded_input_rev.index(tokenizer([":"], return_tensors='pt').input_ids[0, 0].item()) 
+                nll = criterion(output.logits[:, start_ind:, :].transpose(1,2), encoded_answer.input_ids[:, :])
+                # compute perplexity
+                sentence_nll = criterion_sum(output.logits[:, start_ind:, :].transpose(1,2), encoded_answer.input_ids[:, :])
+                ppl = torch.exp(sentence_nll / encoded_answer.input_ids[:, :].shape[-1])
+                
             else:
                 nll = criterion(output.logits.transpose(1,2), encoded_answer.input_ids)
+                # compute perplexity
+                sentence_nll = criterion_sum(output.logits.transpose(1,2), encoded_answer.input_ids)
+                ppl = torch.exp(sentence_nll / encoded_answer.input_ids.shape[-1])
+                
             # store results (probs)
-            probs.append(torch.exp(-nll).item())
-            model_name.append(model)
-            answer_type.append(a)
+            probs[a] = torch.exp(-nll).item()
+            ppls[a] = ppl.item()
+            # normalized_probs[a] = normalized_sentence_prob.item()
+            # probs.append(torch.exp(-nll).item())
+            # ppls.append(ppl.item())
+            # normalized_probs.append(normalized_sentence_prob.item())
+        model_name.append([model]*6)
+        answer_type.append(["taciturn", "competitor", "mostSimilar", "sameCategory", "otherCategory", "fullList"])
+        # average over the different sameCategory / fullList options
+        avg_probs = [probs["taciturn"],
+            probs["competitor"],
+            probs["mostSimilar"],
+            np.mean([probs[k] for k in [c for c in colnames if c.startswith("sameCategory")]]),
+            probs["otherCategory"],
+            np.mean([probs[k] for k in [c for c in colnames if c.startswith("fullList")]])
+        ]
+        avg_ppls = [ppls["taciturn"],
+            ppls["competitor"],
+            ppls["mostSimilar"],
+            np.mean([ppls[k] for k in [c for c in colnames if c.startswith("sameCategory")]]),
+            ppls["otherCategory"],
+            np.mean([ppls[k] for k in [c for c in colnames if c.startswith("fullList")]])
+        ]
+    
         # normalize over the different answer types
-        vignette_probs = probs/np.sum(probs)
+        vignette_probs = avg_probs/np.sum(avg_probs)
         predictions.append(vignette_probs)
+        perplexities.append(avg_ppls)
+    
     # append results to overall df
-    df["answer_type_prob"] = predictions
-    df = df.explode(["answer_type_prob"])
+    df["answer_type_prob_avg"] = predictions
+    df["answer_type_ppl"] = perplexities
     df["model_name"] = model_name
     df["answer_type"] = answer_type
+    df = df.explode(["answer_type_prob_avg", "answer_type_ppl", "model_name", "answer_type"]) 
+    
     
     df.to_csv(output_path)
 
--- code/csv2LLM_data.py
+++ code/csv2LLM_data.py
@@ -5,8 +5,9 @@ for running neural model evaluations.
 import pandas as pd 
 import argparse
 import random
+import itertools
 
-def convert_modular_to_llm_format(path, output):
+def convert_modular_to_llm_format(path, output, expt=1):
     """
     Creates full context strings from modular csvs with alternatives listed separately.
     Also creates all response options in a way taht is maximally advanageous
@@ -23,34 +24,101 @@ def convert_modular_to_llm_format(path, output):
         Path where to save processed file.
     """
     def return_options(row):
-        return [row[i] for i in row["shuffled_options_list"]]
+        opts = []
+        for r in row["shuffled_options_list"]:
+            opts.append([row[i] for i in r])
+        return opts
+
+    def return_options_sameCat(row):
+        opts = []
+        for r in row["shuffled_sameCat_list"]:
+            opts.append([row[i] for i in r])
+        return opts
 
     df = pd.read_csv(path)
 
     df_out = df.copy()
-    # create options string in a set order
-    options = df_out["competitor"] + ", " + df_out["sameCategory"] + ", and " + df_out["otherCategory"] + ". "
-    # shuffle options
-    options_list = [random.sample(["competitor", "sameCategory", "otherCategory"], 3) for i in range(len(df))]
-    df["shuffled_options_list"] = options_list #df.apply(lambda x: random.shuffle(options_list))
-    print("head ", df.head())
-    df["shuffled_options"] = df.apply(lambda x: return_options(x), axis = 1)
-    df["shuffled_options_string"] = df["shuffled_options"].apply(lambda s: ", ".join(s))
-    # put together context
-    df_out["context"] = df_out["vignette_start"] + " " + options + df_out["vignette_continuation"] + " " + df_out["question"] + " You reply:"
-    # replace newline chars (esp relevant for GPT-3)
-    df_out["context"] = df_out["context"].apply(lambda s: s.replace("\\n", ""))
-    # geneare context wihtout the question for the QA models
-    df_out["context_qa"] = df_out["vignette_start"] + " " + options + df_out["vignette_continuation"]
-    df_out["context_qa"] = df_out["context_qa"].apply(lambda s: s.replace("\\n", ""))
-    # construct response options (may require some manual fine-tuning of the taciturn formulation)
-    df_out["taciturn"] = "I'm sorry, we don't have " + df_out["itemQuestion"] + "."
-    df_out["competitor"] = df_out["taciturn"] + " We have " + df["competitor"] + "."
-    df_out["sameCategory"] = df_out["taciturn"] + " We have " + df["competitor"] + " and " + df["sameCategory"] + "."
-    df_out["otherCategory"] = df_out["taciturn"] + " We have " + df["otherCategory"] + "."
-    df_out["fullList"] = df_out["taciturn"] + " We have " + df["shuffled_options_string"] + "."
+    if expt == 1:
+        # create options string in a set order
+        options = df_out["competitor"] + ", " + df_out["sameCategory"] + ", and " + df_out["otherCategory"] + ". "
+        # shuffle options and create all possible full list orders
+        full_list_orders = list(itertools.permutations(["competitor", "sameCategory", "otherCategory"]))
+        # options_list = [random.sample(["competitor", "sameCategory", "otherCategory"], 3) for i in range(len(df))]
+        df["shuffled_options_list"] = [full_list_orders] * len(df) #df.apply(lambda x: random.shuffle(options_list))
+        df["shuffled_options"] = df.apply(lambda x: return_options(x), axis = 1)
+        print("head ", df.head())
+        df["shuffled_options_string"] = df["shuffled_options"].apply(lambda s: [", ".join(i) for i in s])
+        # put together context
+        df_out["context"] = df_out["vignette_start"] + " " + options + df_out["vignette_continuation"] + " " + df_out["question"] + " You reply:"
+        # replace newline chars (esp relevant for GPT-3)
+        df_out["context"] = df_out["context"].apply(lambda s: s.replace("\\n", ""))
+        # geneare context wihtout the question for the QA models
+        df_out["context_qa"] = df_out["vignette_start"] + " " + options + df_out["vignette_continuation"]
+        df_out["context_qa"] = df_out["context_qa"].apply(lambda s: s.replace("\\n", ""))
+        # construct response options (may require some manual fine-tuning of the taciturn formulation)
+        df_out["taciturn"] = "I'm sorry, we don't have " + df_out["itemQuestion"] + "."
+        df_out["competitor"] = df_out["taciturn"] + " We have " + df["competitor"] + "."
+        df_out["sameCategory1"] = df_out["taciturn"] + " We have " + df["competitor"] + " and " + df["sameCategory"] + "."
+        df_out["sameCategory2"] = df_out["taciturn"] + " We have " + df["sameCategory"] + "."
+        df_out["sameCategory3"] = df_out["taciturn"] + " We have " + df["sameCategory"] + " and " + df["competitor"] + "."
+        df_out["otherCategory"] = df_out["taciturn"] + " We have " + df["otherCategory"] + "."
+        for i in range(len(full_list_orders)):
+            df[f"shuffled_options_string_{i}"] = df["shuffled_options_string"].apply(lambda s: s[i])
+            df_out[f"fullList_{i}"] = df_out["taciturn"] + " We have " + df[f"shuffled_options_string_{i}"] + "."
 
-    df_out = df_out[["itemName", "settingName", "context", "context_qa", "question", "taciturn", "competitor", "sameCategory", "otherCategory", "fullList"]]
+        df_out = df_out[["itemName", "settingName", "context", "context_qa", "question", "taciturn", "competitor", "sameCategory1", "sameCategory2", "sameCategory3", "otherCategory", "fullList_0",  "fullList_1",  "fullList_2",  "fullList_3",  "fullList_4",  "fullList_5"]]
+    
+    else:
+        # create options string in a set order
+        options = df_out["competitor"] + ", " + df_out["mostSimilar"] + ", " + df_out["sameCategory"] + ", and " + df_out["otherCategory"] + ". "
+        # shuffle options and create all possible full list orders
+        full_list_orders = list(itertools.permutations(["competitor", "mostSimilar", "sameCategory", "otherCategory"]))
+        df["shuffled_options_list"] = [full_list_orders] * len(df) 
+        df["shuffled_options"] = df.apply(lambda x: return_options(x), axis = 1)
+        print("head ", df.head())
+        df["shuffled_options_string"] = df["shuffled_options"].apply(lambda s: [", ".join(i) for i in s])
+        # same for sameCat 
+        sameCat_list_orders = list(itertools.permutations(["competitor", "mostSimilar", "sameCategory"]))
+        df["shuffled_sameCat_list"] = [sameCat_list_orders] * len(df) 
+        df["shuffled_options_sameCat"] = df.apply(lambda x: return_options_sameCat(x), axis = 1)
+        print("head ", df.head())
+        df["shuffled_sameCat_string"] = df["shuffled_options_sameCat"].apply(lambda s: [", ".join(i) for i in s])
+        
+        # put together context
+        df_out["context"] = df_out["vignette_start"] + " " + options + df_out["vignette_continuation"] + " " + df_out["question"] + " You reply:"
+        # replace newline chars (esp relevant for GPT-3)
+        df_out["context"] = df_out["context"].apply(lambda s: s.replace("\\n", ""))
+        # geneare context wihtout the question for the QA models
+        df_out["context_qa"] = df_out["vignette_start"] + " " + options + df_out["vignette_continuation"]
+        df_out["context_qa"] = df_out["context_qa"].apply(lambda s: s.replace("\\n", ""))
+        # construct response options (may require some manual fine-tuning of the taciturn formulation)
+        df_out["taciturn"] = "I'm sorry, I don't have " + df_out["itemQuestion"] + "."
+        df_out["competitor"] = "No, but I have " + df["competitor"] + "." #  df_out["taciturn"] + 
+        df_out["mostSimilar"] = "No, but I have " + df["mostSimilar"] + "."
+        # add same Cat options
+        for i in range(len(sameCat_list_orders)):
+            df[f"shuffled_sameCat_string_{i}"] = df["shuffled_sameCat_string"].apply(lambda s: s[i])
+            df_out[f"sameCategory_{i}"] = "No, but I have " + df[f"shuffled_sameCat_string_{i}"] + "."
+        # add options with one / two items
+        df_out[f"sameCategory_{i+1}"] = "No, but I have " + df["competitor"] + " and " + df["sameCategory"]  + "."
+        df_out[f"sameCategory_{i+2}"] = "No, but I have " + df["sameCategory"] + " and " + df["competitor"]  + "."
+        df_out[f"sameCategory_{i+3}"] = "No, but I have " + df["sameCategory"] + "."
+        df_out[f"sameCategory_{i+4}"] = "No, but I have " + df["competitor"] + " and " + df["mostSimilar"]  + "."
+        df_out[f"sameCategory_{i+5}"] = "No, but I have " + df["mostSimilar"] + " and " + df["competitor"]  + "."
+        df_out[f"sameCategory_{i+6}"] = "No, but I have " + df["mostSimilar"] + " and " + df["sameCategory"]  + "."
+        df_out[f"sameCategory_{i+7}"] = "No, but I have " + df["sameCategory"] + " and " + df["mostSimilar"]  + "."
+
+        df_out["otherCategory"] = "No, but I have " + df["otherCategory"] + "."
+        for i in range(len(full_list_orders)):
+            df[f"shuffled_options_string_{i}"] = df["shuffled_options_string"].apply(lambda s: s[i])
+            df_out[f"fullList_{i}"] = "No, but I have " + df[f"shuffled_options_string_{i}"] + "."
+
+
+        print("All columns in final df ", df_out.columns)
+        df_out = df_out[df_out.columns[~df_out.columns.isin(['vignette_start','vignette_continuation', 'priorElicitation_context', 'priorElicitation_question', 'answer_template', 'itemQuestion', 'sameCategory', 'competitor_context1',
+       'competitor_context2', 'correct_response'])]]
+        # df_out = df_out[["itemName", "settingName", "context", "context_qa", "question", "taciturn", "competitor", "sameCategory1", "sameCategory2", "sameCategory3", "otherCategory", "fullList_0",  "fullList_1",  "fullList_2",  "fullList_3",  "fullList_4",  "fullList_5"]]
+    
 
     df_out.to_csv(output)
 
@@ -58,7 +126,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-p", "--path", help = "path to file to convert")
     parser.add_argument("-o", "--output", help = "path to save processed output file to")
+    parser.add_argument("-e", "--experiment", help = "number of expt for which data is processed", type=int)
 
     args = parser.parse_args()
 
-    convert_modular_to_llm_format(args.path, args.output)
+    convert_modular_to_llm_format(args.path, args.output, args.experiment)
