ad64bbf75df6e056ca750f4a968a138ad9eaead4
Marcel Klehr <mklehr@gmx.net>
2023-05-12 16:44:45 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
-module.exports = {                                                                                                                                                               7ms 
+module.exports = {                                                                                                                                                    
   configureWebpack: {
     module: {
       rules: [
