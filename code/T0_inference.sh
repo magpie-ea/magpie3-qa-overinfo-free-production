f517ee6dcf392f14fad1022244548a46851a0564
polina-tsvilodub <polina.tsvilodub@gmail.com>
2024-03-12 08:37:58 +0100

    fix some typos

--- code/T0_inference.sh
+++ code/T0_inference.sh
@@ -9,12 +9,13 @@
 module load devel/miniconda/3
 source $MINICONDA_HOME/etc/profile.d/conda.sh
 conda deactivate
-conda activate qa_models
+conda activate llmlink
 echo $(which python)
 module load devel/cuda/11.6
 python QA_models_answer_sampling.py -m="mistralai/Mixtral-8x7B-Instruct-v0.1" \
     -o="../data_paper_neural/results_post_cogsci/trials_LLMs_e1_mixtral-instruct_zero-shot.csv" \
     -p="../experiments/free_production/trials/trials_LLMs_all_options_postprocessed.csv" \
     -topk=5 \
+    -tm=1 \
     -pr="zero-shot" \
     -t="lm_sampling"
