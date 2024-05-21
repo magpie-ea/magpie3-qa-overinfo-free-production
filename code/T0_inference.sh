bc5fedd8f0b6fb75b1b8a9365ed38fb472a14952
polina-tsvilodub <polina.tsvilodub@gmail.com>
2024-05-21 12:49:21 +0200

    add e3 set up for llm results

--- code/T0_inference.sh
+++ code/T0_inference.sh
@@ -14,16 +14,16 @@ module load devel/cuda/11.6
 
 prompts=("zero-shot" "explanation" "example" "cot")
 prompts_e2=("zero-shot" "explanation" "example" "cot")
-expts=("e1" "e2")
+expts=("e3_highprior" "e3_lowprior")
 for j in ${!expts[*]}; do
     echo "Running prompt: ${expts[$j]}"
     case "${expts[$j]}" in 
-        "e2")
+        "e3_highprior")
         for i in ${!prompts_e2[*]}; do
             echo "Running prompt: ${prompts_e2[$i]}"
             python QA_models_answer_sampling.py -m="mistralai/Mixtral-8x7B-Instruct-v0.1" \
                 -o="../data_paper_neural/results_post_cogsci/" \
-                -p="../experiments/contextSensitive_free_production/trials/trials_e2_fctPrompt_fixedOrder.csv" \
+                -p="../experiments/04-priorSensitivity_free_production/trials/trials_pilot2_models.csv" \
                 -ml=128 \
                 -topk=5 \
                 -tm=1 \
@@ -33,12 +33,12 @@ for j in ${!expts[*]}; do
                 -ex="${expts[$j]}"
         done
     ;;
-        "e1")
+        "e3_lowprior")
         for i in ${!prompts[*]}; do
             echo "Running prompt: ${prompts[$i]}"
             python QA_models_answer_sampling.py -m="mistralai/Mixtral-8x7B-Instruct-v0.1" \
                 -o="../data_paper_neural/results_post_cogsci/" \
-                -p="../experiments/free_production/trials/trials_LLMs_all_options_postprocessed.csv" \
+                -p="../experiments/04-priorSensitivity_free_production/trials/trials_pilot2_models.csv" \
                 -ml=128 \
                 -topk=5 \
                 -tm=1 \
