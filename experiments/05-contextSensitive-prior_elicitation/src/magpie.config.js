2315339c80fc1f05093f2789793409386bd79cc3
polina-tsvilodub <polina.tsvilodub@gmail.com>
2024-06-13 16:28:10 +0200

    update completion code

--- experiments/05-contextSensitive-prior_elicitation/src/magpie.config.js
+++ experiments/05-contextSensitive-prior_elicitation/src/magpie.config.js
@@ -3,7 +3,7 @@ export default {
   serverUrl: 'https://magpie-cogsciprag.fly.dev//',
   socketUrl: 'wss://magpie-cogsciprag.fly.dev//socket',
   // this will be used in prolific mode
-  completionUrl: 'https://app.prolific.co/submissions/complete?cc=CT0J8WXX',
+  completionUrl: 'https://app.prolific.com/submissions/complete?cc=CT0J8WXX',
   contactEmail: 'polina.tsvilodub@uni-tuebingen.de',
   // Either 'debug', 'directLink' or 'prolific'
   mode: 'prolific',
